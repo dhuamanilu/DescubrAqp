bf259a673296e8c3e8e4059742e1766b6bda4ea7
eazurin <eazurin@unsa.edu.pe>
2024-11-12 16:21:30 -0500

    Boton logout

--- app/src/main/java/com/example/lab4_fragments/fragments/HomeFragment.java
+++ app/src/main/java/com/example/lab4_fragments/fragments/HomeFragment.java
@@ -1,16 +1,19 @@
 package com.example.lab4_fragments.fragments;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import com.example.lab4_fragments.LoginActivity;
 
 import com.example.lab4_fragments.R;
 
@@ -18,8 +21,8 @@ public class HomeFragment extends Fragment {
 
     private ImageView mapImage;
     private Button viewUbiButton;
+    private Button logoutButton;
     private boolean isShowingMap = true;
-
 
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
@@ -42,10 +45,18 @@ public class HomeFragment extends Fragment {
 
         mapImage = view.findViewById(R.id.mapImage);
         viewUbiButton = view.findViewById(R.id.viewUbi);
+        logoutButton = view.findViewById(R.id.logoutButton);
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", getActivity().MODE_PRIVATE);
+        String loggedInUser = sharedPreferences.getString("loggedInUser", "Usuario");
+
+        Toast.makeText(getActivity(), "Bienvenido, " + loggedInUser, Toast.LENGTH_SHORT).show();
 
         viewUbiButton.setOnClickListener(v -> toggleImage());
 
         mapImage.setOnClickListener(v -> navigateToDetailFragment());
+
+        logoutButton.setOnClickListener(v -> logout());
     }
 
     private void toggleImage() {
@@ -64,5 +75,18 @@ public class HomeFragment extends Fragment {
         transaction.replace(R.id.fragmentContainerView, new DetailFragment());
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+
+    private void logout() {
+        // Eliminar el usuario logueado de SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", getActivity().MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("loggedInUser");  
+        editor.apply();
+
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
